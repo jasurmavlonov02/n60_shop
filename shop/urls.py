9db411cf10d4b1894a26ef339d48c2650c315389
jasurmavlonov02 <jm1495046@gmail.com>
2025-02-27 14:22:42 +0500

    products_of_category

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,7 @@ from shop import views
 
 urlpatterns = [
     path('home/', views.index, name='index'),
-    path('product-detail/<int:product_id>/',views.product_detail, name='product_detail')
+    path('product-detail/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('products-of-category/<int:category_id>/', views.index, name='products_of_category')
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
-from shop.models import Product
+from shop.models import Product, Category
 
 
 # Create your views here.
 
 
-def index(request):
-    products = Product.objects.all().order_by('-updated_at') # select * from products order by updated_at DESC
+def index(request, category_id: int | None = None):
+    categories = Category.objects.all()
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all().order_by('-updated_at')  # select * from products order by updated_at DESC
     context = {
-        'products': products
+        'products': products,
+        'categories': categories
     }
     return render(request, 'shop/home.html', context)
 
