2f65061bdc729464c471f3a4d53731bac5b21721
jmavlonov <jasurmavloniy24@gmail.com>
2025-03-11 15:53:30 +0500

    123

--- core/settings.py
+++ core/settings.py
@@ -132,3 +132,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'shop/media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
